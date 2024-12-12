9b5989d09ba4ecf7bbc46d5c76e2d9760239189b
Gert Jan Schoneveld <gj_schoneveld@ymail.com>
2024-12-12 20:51:18 +0100

    Day 12

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -3,7 +3,8 @@ using Point = (int X, int Y);
 
 Map map = File.ReadAllLines("input.txt");
 
-var totalPrice = 0; 
+var totalPrice = 0;
+var discountedPrice = 0;
 var visited = new HashSet<Point>();
 
 for (int y = 0; y < map.Length; y++)
@@ -17,41 +18,104 @@ for (int y = 0; y < map.Length; y++)
 
         var symbol = map[y][x];
         var area = 0;
-        var perimeter = 0;
 
         var toVisit = new Queue<Point>();
         toVisit.Enqueue((x, y));
+
+        var edges = new List<(Point Position, Direction Direction)>();
 
         while(toVisit.Count > 0)
         {
             var current = toVisit.Dequeue();
             visited.Add(current);
 
-            var neighbours = Neighbours(current).Where(nb => IsValid(map, nb) && map[nb.Y][nb.X] == symbol).ToList();
-            perimeter += 4 - neighbours.Count;
+            var neighbours = Neighbours(current).Where(nb => IsValid(map, nb.Position) && map[nb.Position.Y][nb.Position.X] == symbol).ToList();
+
+            var directions = new List<Direction>
+            {
+                Direction.North,
+                Direction.South,
+                Direction.West,
+                Direction.East
+            };
+
+            edges.AddRange(directions.Where(d => !neighbours.Any(nb => nb.Direction == d)).Select(d => (current, d)));
             area++;
 
-            foreach (var nb in neighbours.Where(nb => !visited.Contains(nb) && !toVisit.Contains(nb)))
+            foreach (var nb in neighbours.Where(nb => !visited.Contains(nb.Position) && !toVisit.Contains(nb.Position)))
             {
-                toVisit.Enqueue(nb);
+                toVisit.Enqueue(nb.Position);
             }
         }
 
-        totalPrice += area * perimeter;
+        totalPrice += area * edges.Count;
+        discountedPrice += area * CountSides(edges);
     }
 }
 
 var answer1 = totalPrice;
 Console.WriteLine($"Answer 1: {answer1}");
 
+var answer2 = discountedPrice;
+Console.WriteLine($"Answer 2: {answer2}");
 
-List<Point> Neighbours(Point position)
+int CountSides(List<(Point Position, Direction Direction)> edges)
+{
+    return CountSidesVertical(edges, Direction.West) +
+        CountSidesVertical(edges, Direction.East) +
+        CountSidesHorizontal(edges, Direction.North) +
+        CountSidesHorizontal(edges, Direction.South);
+}
+
+int CountSidesVertical(List<(Point Position, Direction Direction)> edges, Direction direction)
+{
+    int sides = 0;
+
+    var positions = edges.Where(e => e.Direction == direction).Select(e => e.Position).OrderBy(e => e.X).ThenBy(e => e.Y).ToList();
+
+    Point previous = (-1, -1);
+
+    foreach (var position in positions)
+    {
+        if (position.X != previous.X || position.Y - previous.Y != 1)
+        {
+            sides++;
+        }
+
+        previous = position;
+    }
+
+    return sides;
+}
+
+int CountSidesHorizontal(List<(Point Position, Direction Direction)> edges, Direction direction)
+{
+    int sides = 0;
+
+    var positions = edges.Where(e => e.Direction == direction).Select(e => e.Position).OrderBy(e => e.Y).ThenBy(e => e.X).ToList();
+
+    Point previous = (-1, -1);
+
+    foreach (var position in positions)
+    {
+        if (position.Y != previous.Y || position.X - previous.X != 1)
+        {
+            sides++;
+        }
+
+        previous = position;
+    }
+
+    return sides;
+}
+
+List<(Point Position, Direction Direction)> Neighbours(Point position)
 {
     return [
-        (position.X - 1, position.Y),
-        (position.X + 1, position.Y),
-        (position.X, position.Y - 1),
-        (position.X, position.Y + 1)
+        ((position.X - 1, position.Y), Direction.West),
+        ((position.X + 1, position.Y), Direction.East),
+        ((position.X, position.Y - 1), Direction.North),
+        ((position.X, position.Y + 1), Direction.South)
     ];
 }
 
@@ -62,4 +126,12 @@ bool IsValid(Map map, Point position)
 
     return 0 <= position.X && position.X < lengthX &&
         0 <= position.Y && position.Y < lengthY;
+}
+
+enum Direction
+{
+    North,
+    South,
+    West,
+    East
 }
