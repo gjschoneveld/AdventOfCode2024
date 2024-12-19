ee5d68c6cd255eb30743c1ad375d4a745cc236bd
Gert Jan Schoneveld <gj_schoneveld@ymail.com>
2024-12-19 21:02:05 +0100

    Day 19

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -2,7 +2,7 @@
 var patterns = input[0].Split(", ").ToList();
 var designs = input[2..];
 
-var ways = designs.Select(d => CountDifferentWays(patterns, [], d, 0)).ToList();
+var ways = designs.Select(d => CountDifferentWays(patterns, d, 0, [])).ToList();
 
 var answer1 = ways.Count(w => w > 0);
 Console.WriteLine($"Answer 1: {answer1}");
@@ -10,7 +10,7 @@ Console.WriteLine($"Answer 1: {answer1}");
 var answer2 = ways.Sum();
 Console.WriteLine($"Answer 2: {answer2}");
 
-static long CountDifferentWays(List<string> patterns, Dictionary<int, long> cache, string design, int index)
+static long CountDifferentWays(List<string> patterns, string design, int index, Dictionary<int, long> cache)
 {
     if (index == design.Length)
     {
@@ -22,27 +22,9 @@ static long CountDifferentWays(List<string> patterns, Dictionary<int, long> cach
         return cache[index];
     }
 
-    var ways = 0L;
-    var maxLength = design.Length - index;
+    var ways = patterns
+        .Where(p => index + p.Length <= design.Length && design.Substring(index, p.Length) == p)
+        .Sum(p => CountDifferentWays(patterns, design, index + p.Length, cache));
 
-    foreach (var pattern in patterns)
-    {
-        if (pattern.Length > maxLength)
-        {
-            continue;
-        }
-
-        var substring = design.Substring(index, pattern.Length);
-
-        if (pattern != substring)
-        {
-            continue;
-        }
-
-        ways += CountDifferentWays(patterns, cache, design, index + pattern.Length);        
-    }
-
-    cache[index] = ways;
-
-    return ways;
+    return cache[index] = ways;
 }
