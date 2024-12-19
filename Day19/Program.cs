e73095c06fb9a733b7f2f2888871d8632fcedccb
Gert Jan Schoneveld <gj_schoneveld@ymail.com>
2024-12-19 20:36:01 +0100

    Day 19

---
+++ Day19/Program.cs
@@ -0,0 +1,45 @@
+﻿var input = File.ReadAllLines("input.txt");
+var patterns = input[0].Split(", ").ToList();
+var designs = input[2..];
+
+var answer1 = designs.Count(d => IsValid(patterns, [], d, 0));
+Console.WriteLine($"Answer 1: {answer1}");
+
+static bool IsValid(List<string> patterns, HashSet<int> invalid, string design, int index)
+{
+    if (index == design.Length)
+    {
+        return true;
+    }
+
+    if (invalid.Contains(index))
+    {
+        return false;
+    }
+
+    var maxLength = design.Length - index;
+
+    foreach (var pattern in patterns)
+    {
+        if (pattern.Length > maxLength)
+        {
+            continue;
+        }
+
+        var substring = design.Substring(index, pattern.Length);
+
+        if (pattern != substring)
+        {
+            continue;
+        }
+
+        if (IsValid(patterns, invalid, design, index + pattern.Length))
+        {
+            return true;
+        }            
+    }
+
+    invalid.Add(index);
+
+    return false;
+}
